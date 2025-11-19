fc66173bfce35917c99a4769f7a3a13a27047d4e
jdguzmanc23 <jd.guzmanc12@uniandes.edu.co>
2025-11-19 06:57:37 -0500

    Punto 1 modules

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { ToastrModule } from 'ngx-toastr';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpErrorInterceptorService } from './interceptors/http-error-interceptor.service';
 import { MovieModule } from './movie/movie.module';
 
@@ -21,6 +21,7 @@ import { MovieModule } from './movie/movie.module';
     MovieModule,
   ],
   providers: [
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HttpErrorInterceptorService,
--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { ActorModule } from '../actor/actor.module';
+import { MovieService } from './movie.service';
 
 @NgModule({
   declarations: [MovieListComponent, MovieDetailComponent],
   imports: [CommonModule, ActorModule],
   exports: [MovieListComponent],
+  providers: [MovieService],
 })
 export class MovieModule {}
