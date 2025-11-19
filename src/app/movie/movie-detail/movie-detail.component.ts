45ab96d7603604e1aa0728e8527adecd0a6609a5
jdguzmanc23 <jd.guzmanc12@uniandes.edu.co>
2025-11-19 07:06:53 -0500

    PUNTO 2

--- src/app/movie/movie-detail/movie-detail.component.ts
+++ src/app/movie/movie-detail/movie-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from '../Movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,6 +10,26 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './movie-detail.component.css',
 })
 export class MovieDetailComponent implements OnInit {
-  @Input() movie: any;
-  ngOnInit(): void {}
+  movie: Movie | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private movieService: MovieService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.movieService.getMovieDetail(Number(id)).subscribe({
+        next: (data) => {
+          this.movie = data;
+        },
+        error: (error) => {
+          console.error('Error loading movie detail:', error);
+          this.router.navigate(['/movies']);
+        }
+      });
+    }
+  }
 }
--- src/app/movie/movie-list/movie-list.component.ts
+++ src/app/movie/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Movie } from '../Movie';
-import { moviesData } from '../moviesData';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,15 +11,24 @@ import { moviesData } from '../moviesData';
 })
 export class MovieListComponent implements OnInit {
   movies: Movie[] = [];
-  selected: Boolean = false;
-  selectedMovie: Movie | null = null;
+
+  constructor(
+    private movieService: MovieService,
+    private router: Router
+  ) {}
 
   onSelect(movie: Movie) {
-    this.selectedMovie = movie;
-    this.selected = true;
+    this.router.navigate(['/movies', movie.id]);
   }
 
   ngOnInit() {
-    this.movies = moviesData;
+    this.movieService.getMovies().subscribe({
+      next: (data) => {
+        this.movies = data;
+      },
+      error: (error) => {
+        console.error('Error loading movies:', error);
+      }
+    });
   }
 }
